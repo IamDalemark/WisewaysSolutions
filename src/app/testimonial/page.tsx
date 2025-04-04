290f016df8664a17a7f46618fbb9b52ae84d2e96
IamDalemark <danielweg123@gmail.com>
2025-04-05 01:03:57 +0800

    add testimonial frontend

--- src/app/testimonial/page.tsx
+++ src/app/testimonial/page.tsx
@@ -1,10 +1,26 @@
+"use client";
+import { useState } from "react";
 import SendTestimonial from "../../components/testimonial/SendTestimonial";
-
+import SentTestimonial from "@/components/testimonial/SentTestimonial";
+import { useRouter } from "next/navigation";
 const Testimonial = () => {
+  const router = useRouter();
+  const [isSubmitted, setIsSubmitted] = useState(false);
+  const handleSubmit = () => {
+    setIsSubmitted(true);
+  };
+
+  const handleBackToHome = () => {
+    router.push("/");
+  };
   return (
     <div className="bg-gray-white h-[100vh] w-full ">
       nav bar goes here
-      <SendTestimonial />
+      {isSubmitted ? (
+        <SentTestimonial onSubmit={handleBackToHome} />
+      ) : (
+        <SendTestimonial onSubmit={handleSubmit} />
+      )}
     </div>
   );
 };
