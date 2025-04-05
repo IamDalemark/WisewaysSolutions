173d21e61c56304b74a05a348ae36b9f7dcf8690
jpross14 <dallyeoartemis@gmail.com>
2025-04-05 23:03:53 +0800

    [JP] init landing page and finsihed basic design of navbar

--- src/components/layout/NavBar.tsx
+++ src/components/layout/NavBar.tsx
@@ -1,40 +1,42 @@
+"use client";
+
 import React from "react";
-import logo from "../assets/wiseways_logo.png";
 import { ChevronDown, User } from "lucide-react";
 import Image from "next/image";
-// import "./NavBar.css";
+import DropDownMenuItem from "./DropDownMenuItem";
 
 export const NavBar = () => {
     return (
-    <nav className="flex justify-start items-center bg-[#F3F3F3] sticky top-0 ml-[10%] mr-[10%] mt-6 rounded-3xl h-20">
-        <Image src="/assets/wiseways_logo.png" alt="WiseWays Solution logo" width={340} height={103} className="w-1/5"></Image>
+    <nav className="flex justify-start items-center bg-[#F3F3F3] sticky top-6 ml-[10%] mr-[10%] rounded-3xl h-20 shadow-xl">
+        <Image src={"/wiseways_navbar_logo.png"} alt="WiseWays Solution logo" width={190} height={51.5} className="mx-5 my-1"></Image>
 
         <ul className="flex">
             <li className="list-none">
-                <a href="" className="flex text-[#0D767A] p-2 mx-2 hover:text-[#FD8432] hover:underline">
-                    SERVICES<ChevronDown color="#086B70" strokeWidth={3}/>
-                </a>
+                <DropDownMenuItem label="SERVICES"/>
+                {/* <a href="" className="flex text-[#0D767A] p-2 mx-2 hover:text-[#FD8432] hover:underline">
+                    SERVICES
+                </a> */}
             </li>
             <li className="list-none">
-                <a href="" className="flex text-[#0D767A] p-2 mx-2 hover:text-[#FD8432] hover:underline">
+                <a href="#about" className="flex text-[#0D767A] p-2 mx-2 hover:text-[#FD8432] hover:underline">
                     ABOUT
                 </a>
             </li>
             <li className="list-none">
-                <a href="" className="flex text-[#0D767A] p-2 mx-2 hover:text-[#FD8432] hover:underline">
+                <a href="#contact" className="flex text-[#0D767A] p-2 mx-2 hover:text-[#FD8432] hover:underline">
                     CONTACT
                 </a>
             </li>
         </ul>
 
-        <button className="inline-block h-5/8 w-1/7 py-1 bg-[#0D767A] text-[#F3F3F3] rounded-2xl mx-3 leading-[1.25] 
+        <button className="inline-block h-5/8 w-1/6 py-1 bg-[#0D767A] text-[#F3F3F3] rounded-2xl ml-56 mr-1 leading-[1.25] 
         hover:bg-[#086B70] hover:text-[#BECECE]">
-        <p>SCHEDULE APPOINTMENT</p>
+        <p>SCHEDULE <br /> APPOINTMENT</p>
         </button>
 
-        <div className="flex h-9 w-18 rounded-[2.5rem] mx-10 hover:bg-[#D7D8D8]">
-            <div className="bg-[#BECECE] h-full w-1/2 rounded-full p-1.5"><User color="#5D7F81" strokeWidth={2.5}/></div>
-            <ChevronDown color="#086B70" strokeWidth={3} size={32} className="p-1"/>
+        <div className="flex h-11 w-22 rounded-[2.5rem] ml-5 mr-9 hover:bg-[#D7D8D8]">
+            <div className="bg-[#BECECE] h-full w-1/2 rounded-full p-1.5"><User color="#5D7F81" strokeWidth={2.5} size={30} className="m-auto"/></div>
+            <ChevronDown color="#086B70" strokeWidth={3} size={32} className="m-auto ml-0.5"/>
         </div>
     </nav>
 
@@ -42,4 +44,4 @@ export const NavBar = () => {
 };
 
 
-export default NavBar
+export default NavBar;
