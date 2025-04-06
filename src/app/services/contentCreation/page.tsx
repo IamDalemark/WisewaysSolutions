9ac5f966fee5364f54e205d2b30751da97f2fb6d
prizhera <141991133+prizhera@users.noreply.github.com>
2025-04-06 22:30:15 +0800

    Revert "Add initial pages for the service pages"

--- src/app/services/contentCreation/page.tsx
+++ src/app/services/contentCreation/page.tsx
@@ -1,36 +1,5 @@
-import Image from "next/image";
-import NavBar from "@/components/layout/NavBar";
-
 const ContentCreationPage = () => {
-  return (
-    <div className="bg-[#E1E1E1]"> 
-      <NavBar /> 
-
-      <main className="text-[#0D767A] min-h-screen pt-24 px-6 md:px-16">
-        <section className="flex flex-col md:flex-row items-center justify-between gap-8 max-w-7xl mx-auto">
-          <div className="flex-1 text-center md:text-left">
-            <h1 className="text-4xl md:text-5xl font-bold mb-4">
-              Content Creation
-            </h1>
-            <p className="text-lg md:text-xl">
-            Blogs, videos, social media posts… basically making companies look smarter and cooler online.
-            </p>
-          </div>
-
-          <div className="flex-1">
-            <Image
-              src="/content.png"
-              alt="Accounting illustration"
-              className="rounded-xl object-cover"
-              width={500}
-              height={350}
-              priority
-            />
-          </div>
-        </section>
-      </main>
-    </div>
-  );
+  return <div>this is the content creation page</div>;
 };
 
 export default ContentCreationPage;
--- src/app/services/customerSupport/page.tsx
+++ src/app/services/customerSupport/page.tsx
@@ -1,37 +1,5 @@
-import Image from "next/image";
-import NavBar from "@/components/layout/NavBar";
-
 const CustomerSupportPage = () => {
-  return (
-    <div className="bg-[#E1E1E1]"> 
-      <NavBar /> 
-
-      <main className="text-[#0D767A] min-h-screen pt-24 px-6 md:px-16">
-        <section className="flex flex-col md:flex-row items-center justify-between gap-8 max-w-7xl mx-auto">
-          <div className="flex-1 text-center md:text-left">
-            <h1 className="text-4xl md:text-5xl font-bold mb-4">
-              Customer Support
-            </h1>
-            <p className="text-lg md:text-xl">
-            Call centers, chat support, and email handling. The frontline of
-            Let me transfer you to another department.
-            </p>
-          </div>
-
-          <div className="flex-1">
-            <Image
-              src="/customer.png"
-              alt="Accounting illustration"
-              className="rounded-xl object-cover"
-              width={500}
-              height={350}
-              priority
-            />
-          </div>
-        </section>
-      </main>
-    </div>
-  );
+  return <div>this is the content creation page</div>;
 };
 
 export default CustomerSupportPage;
