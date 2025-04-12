3ed7c8a9f43e5064690034b326457b6f48750633
IamDalemark <danielweg123@gmail.com>
2025-04-12 23:55:08 +0800

    small fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,10 @@
 "use client";
-
-import { useEffect, useState } from "react";
-import { supabase } from "@/lib/supabaseClient";
 import LandingPage from "./pages/LandingPage";
 
 export default function Page() {
   return (
     <div className="scroll-smooth">
       <LandingPage />
-      <h1>{status}</h1>
-      this is the landing page
     </div>
   );
 }
