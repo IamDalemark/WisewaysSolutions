a8c5b9e3794383c4facfd57e9c8d45d65b10f415
IamDalemark <danielweg123@gmail.com>
2025-04-20 23:24:30 +0800

    fix: storybook config

--- src/components/testimonial/Testimonial.tsx
+++ src/components/testimonial/Testimonial.tsx
@@ -3,7 +3,7 @@ import { TestimonialProps } from "@/types/testimonials.type";
 
 const Testimonial = ({ testimonial, name, ratings }: TestimonialProps) => {
   return (
-    <div className="grid grid-rows-7 w-[60vw] md:w-[40vw] lg:w-[20vw] h-[60vh] border-[#FD8432] border p-4 rounded-2xl sm:m-2 justify-self-center mt-10 ">
+    <div className="grid grid-rows-7 w-[60vw] md:w-[40vw] lg:w-[20vw] h-[60vh] border-[#FD8432] border p-4 rounded-2xl sm:m-2 justify-self-center mt-10  text-blue-green">
       <div className="row-span-5 mt-1 overflow-y-auto pr-2">{testimonial}</div>
       <div className="row-span-2 grid grid-cols-4 mt-2 ">
         <div className="">
