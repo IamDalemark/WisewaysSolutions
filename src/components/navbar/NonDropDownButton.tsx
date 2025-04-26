7f5e13a539a236793e3c61c336553940bb4d2ac8
jpross14 <dallyeoartemis@gmail.com>
2025-04-26 15:03:18 +0800

    modified navbar buttons' scrolling feature by implementing actual scrolling effect

--- src/components/navbar/NonDropDownButton.tsx
+++ src/components/navbar/NonDropDownButton.tsx
@@ -1,5 +1,5 @@
 // import Link from "next/link";
-import { useRouter } from "next/navigation";
+import { useRouter, usePathname } from "next/navigation";
 
 interface Props {
     section: string;
@@ -10,8 +10,22 @@ const NonDropDownButton = ({ section, label }: Props) => {
     const baseClass = "flex text-xl lg:text-base text-blue-green p-2 hover:text-[#FD8432] hover:scale-105 transition-all cursor-pointer";
     
     const router = useRouter();
+    const pathname = usePathname();
+
     const handleClick = () => {
-        router.push(section);
+        if (pathname === "/") {
+            // If already on landing page, manually scroll to section
+            const target = document.getElementById(section);
+            if (target) {
+                const yOffset = -120;
+                const y = target.getBoundingClientRect().top + window.pageYOffset + yOffset;
+                window.scrollTo({ top: y, behavior: "smooth" });
+            }
+        } else {
+        // If from another page, save target section temporarily, then redirect to landing
+        sessionStorage.setItem("scrollTarget", section);
+        router.push("/");
+        }
     };
     
     return (
