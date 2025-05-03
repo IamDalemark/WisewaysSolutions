6fdd9eaad28780ce45db235bfb7dc5bb8c724d9b
jpross14 <dallyeoartemis@gmail.com>
2025-05-03 16:00:54 +0800

    style: minor change to tailwind styling and minor cleanup of whitespaces in code

--- src/components/booking/BookingLandingContent.tsx
+++ src/components/booking/BookingLandingContent.tsx
@@ -13,14 +13,15 @@ const BookingLandingContent = ({
   const firstDayOfMonth = startOfMonth(currentDate).getDay();
   return (
     <div className="flex items-center justify-center min-h-[calc(100vh-100px)] px-8">
-      <div className="flex flex-col lg:flex-row items-start justify-center gap-20 max-w-7xl w-full">
+      <div className="flex flex-col lg:flex-row items-start justify-center gap-5 max-w-7xl w-full">
         {/* Left Content */}
         <div className="flex flex-col max-w-xl w-full">
           <h1 className="text-teal-700 text-5xl font-bold leading-tight mb-8">
             Lorem ipsum dolor sit amet, consectetur
           </h1>
           <button
-            className="bg-teal-700 text-white rounded-2xl shadow-md px-6 py-4 text-sm font-semibold w-fit"
+            className="bg-teal-700 text-white rounded-2xl shadow-md px-6 py-4 text-base font-semibold 
+            w-fit hover:bg-blue-green-dark hover:scale-103 transition-all cursor-pointer"
             onClick={onStartBookingProcess}
           >
             CHOOSE A DATE & TIME
@@ -28,7 +29,7 @@ const BookingLandingContent = ({
         </div>
 
         {/* Right Calendar */}
-        <div className="bg-white p-6 rounded-3xl shadow-md w-[350px]">
+        <div className="bg-white p-6 rounded-3xl shadow-md h-full w-[350px]">
           <div className="flex justify-center items-center py-2">
             <span className="text-lg font-semibold">
               {format(currentDate, "MMMM yyyy")}
--- src/components/navbar/NavBarWrapper.tsx
+++ src/components/navbar/NavBarWrapper.tsx
@@ -3,13 +3,11 @@
 import { usePathname } from "next/navigation";
 import NavBar from "./NavBar";
 
-const hiddenPaths = ["/admin"];
-
 export default function NavBarWrapper() {
     const pathname = usePathname();
-    const adminRoute = pathname.startsWith("/admin")
+    const adminRoute = pathname.startsWith("/admin");
     if (adminRoute) {
-        return null
+        return null;
     };
     return <NavBar/>;
 }
