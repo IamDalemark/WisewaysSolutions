89a9588cccb7ca5d73707c10900813f39fcbe7d5
FrancyAmada <francyamada1983@gmail.com>
2025-05-10 16:23:17 +0800

    feat: added logout hook

---
+++ src/app/hooks/auth/useLogout.ts
@@ -0,0 +1,33 @@
+import { useState } from "react";
+import { LogOutResult } from "@/types/auth.type";
+import { supabase } from "@/lib/supabaseClient";
+
+export const useLogout = () => {
+  const [logOutLoading, setLogOutLoading] = useState(false);
+
+  const logOut = async (): Promise<LogOutResult> => {
+    setLogOutLoading(true);
+    try {
+      const { error } = await supabase.auth.signOut({ scope: "local" });
+
+      if (error) {
+        throw new Error(error.message || "Failed to Log Out.");
+      }
+
+      console.log(error);
+      return { success: true };
+    } catch (error) {
+      return {
+        success: false,
+        error:
+          error instanceof Error
+            ? error.message
+            : "Failed to Log Out. Please try again.",
+      };
+    } finally {
+      setLogOutLoading(false);
+    }
+  };
+
+  return { logOut, logOutLoading };
+};
