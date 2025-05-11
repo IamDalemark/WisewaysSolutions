c593041898cc304d3720d0479d52f08ddf3fab99
IamDalemark <danielweg123@gmail.com>
2025-05-11 23:19:39 +0800

    chore: email to admin for deploy

--- src/lib/mailerSend.ts
+++ src/lib/mailerSend.ts
@@ -3,4 +3,7 @@ import { MailerSend, Sender } from "mailersend";
 export const mailerSend = new MailerSend({
   apiKey: process.env.MAILER_SEND_KEY!,
 });
-export const domain = new Sender(process.env.DOMAIN!, "Wiseways Solutions");
+export const domain = new Sender(
+  process.env.ADMIN_EMAIl!,
+  "Wiseways Solutions"
+);
