ba3c441fc375f9c655a9a2dca9f8845a4ae27957
IamDalemark <danielweg123@gmail.com>
2025-05-27 13:01:15 +0800

    test: update jest test for testimonial

--- src/__tests__/testimonialRoute.test.tsx
+++ src/__tests__/testimonialRoute.test.tsx
@@ -1,6 +1,13 @@
+import { supabase } from "@/lib/supabaseClient";
+const route = "http://localhost:3000/api/testimonials";
+
+beforeEach(async () => {
+  await supabase.from("testimonial").delete().not("testimonial_id", "is", null);
+});
+
 describe("POST /api/testimonials", () => {
   it("should return 201 when all required fields are present", async () => {
-    const response = await fetch("http://localhost:3000/api/testimonials", {
+    const response = await fetch(route, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
@@ -8,6 +15,7 @@ describe("POST /api/testimonials", () => {
       body: JSON.stringify({
         name: "John Doe",
         email: "john@example.com",
+        title: "CEO",
         rating: 5,
         testimonial: "Awesome service!",
       }),
@@ -16,11 +24,26 @@ describe("POST /api/testimonials", () => {
     const json = await response.json();
     expect(response.status).toBe(201);
     expect(json.success).toBe(true);
-    expect(json.data).toBeDefined();
+    expect(json.data[0]).toEqual(
+      expect.objectContaining({
+        testimonial_id: expect.stringMatching(
+          /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/
+        ),
+        submitted_at: expect.stringMatching(
+          /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d+(\+\d{2}:\d{2}|Z)$/
+        ),
+        name: "John Doe",
+        testimonial: "Awesome service!",
+        rating: "5",
+        is_approved: "pending",
+        email: "john@example.com",
+        title: "CEO",
+      })
+    );
   });
 
   it("should return 400 if required fields are missing", async () => {
-    const response = await fetch("http://localhost:3000/api/testimonials", {
+    const response = await fetch(route, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
@@ -33,7 +56,7 @@ describe("POST /api/testimonials", () => {
   });
 
   it("should return 400 when email is invalid", async () => {
-    const response = await fetch("http://localhost:3000/api/testimonials", {
+    const response = await fetch(route, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
@@ -41,6 +64,7 @@ describe("POST /api/testimonials", () => {
       body: JSON.stringify({
         name: "Invalid Email User",
         email: "not-an-email",
+        title: "CEO",
         rating: 4,
         testimonial: "Good stuff!",
       }),
