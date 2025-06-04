fec9558d99f6e318a9cb4bbf62cd1ac461802022
IamDalemark <danielweg123@gmail.com>
2025-06-05 05:41:18 +0800

    fix: envs

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,6 +1,9 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL_TEST!;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY_TEST!;
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+
+// const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL_TEST!;
+// const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY_TEST!;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
--- src/lib/testSupabase.ts
+++
@@ -1,6 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-
-const testSupabaseUrl = process.env.TEST_SUPABASE_URL!;
-const testSupabaseAnonKey = process.env.TEST_SUPABASE_ANON_KEY!;
-
-export const testSupabase = createClient(testSupabaseUrl, testSupabaseAnonKey);
